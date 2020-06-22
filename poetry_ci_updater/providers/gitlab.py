747389eb9d63a0a3cbd75bf28e059a366286a22e
MarcoGlauser <marco.glauser@gmail.com>
2020-06-22 20:40:26 +0200

    add comment when the merge request already exists on gitlab

--- poetry_ci_updater/providers/gitlab.py
+++ poetry_ci_updater/providers/gitlab.py
@@ -11,8 +11,22 @@ class Gitlab(Provider):
     def run(self):
         self.token = os.getenv('CI_JOB_TOKEN')
         self.project_id = os.getenv('CI_PROJECT_ID')
-        if self.search_for_merge_request() < 1:
+        merge_request = self.search_for_merge_request()
+        if merge_request:
+            self.add_note(merge_request)
+        else:
             self.create_merge_request()
+
+    def add_note(self, merge_request):
+        url = f'https://gitlab.com/api/v4/projects/{self.project_id}/merge_requests/{merge_request["iid"]}/notes'
+        headers = {
+            'Authorization': f'Bearer {self.token}'
+        }
+        data = {
+            'body': self.updates_string()
+        }
+        response = requests.post(url, json=data, headers=headers)
+        response.raise_for_status()
 
     def search_for_merge_request(self):
         url = f'https://gitlab.com/api/v4/projects/{self.project_id}/merge_requests'
@@ -20,12 +34,18 @@ class Gitlab(Provider):
             'Authorization': f'Bearer {self.token}'
         }
         params = {
-            'state': 'opened'
+            'state': 'opened',
+            'source_branch': self.branch_name,
+            'search': self.merge_request_title,
+            'in': 'title'
         }
         response = requests.get(url, params=params, headers=headers)
         response.raise_for_status()
-        open_merge_requests = [True for merge_request in response.json() if merge_request['title'] == self.merge_request_title]
-        return len(open_merge_requests)
+        open_merge_requests = response.json()
+        try:
+            return open_merge_requests[0]
+        except KeyError:
+            return None
 
     def create_merge_request(self):
         url = f"https://gitlab.com/api/v4/projects/{self.project_id}/merge_requests"
@@ -41,4 +61,5 @@ class Gitlab(Provider):
             'remove_source_branch': True,
             'squash': True,
         }
-        requests.post(url, json=data, headers=headers)
+        response = requests.post(url, json=data, headers=headers)
+        response.raise_for_status()
